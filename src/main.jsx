20afe8b7e68b6ff2697d365eb95ee8a28748a2a9
arimumariam <mariamarimu24@gmail.com>
2026-04-10 17:32:53 +0300

    fixed paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// Render the mariam.jsx component from the project root
-import Mariam from '../mariam.jsx'
+// Render the mariam.jsx component from the src directory
+import Mariam from './mariam.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
